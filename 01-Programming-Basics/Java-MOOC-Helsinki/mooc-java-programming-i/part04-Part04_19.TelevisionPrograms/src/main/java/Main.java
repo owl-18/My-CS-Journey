aa1a09014683df5b7a806326f2b872196b67ee05
owl-18 <twoblibion18@gmail.com>
2026-03-16 01:04:38 +0700

    feat: complete TelevisionProgram execrise

--- 01-Programming-Basics/Java-MOOC-Helsinki/mooc-java-programming-i/part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
+++ 01-Programming-Basics/Java-MOOC-Helsinki/mooc-java-programming-i/part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
@@ -9,5 +9,27 @@ public class Main {
         ArrayList<TelevisionProgram> programs = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+            if (name.isEmpty()) {
+                break;
+            }
+            System.out.print("Duration: ");
+            int duration = Integer.valueOf(scanner.nextLine());
+            programs.add(new TelevisionProgram(name, duration));
+        }
+
+        System.out.println();
+        System.out.print("Program's maximum duration? ");
+        int maximumDuration = Integer.valueOf(scanner.nextLine());
+
+        System.out.println();
+        for (TelevisionProgram p: programs) {
+            if (p.getDuration() <= maximumDuration) {
+                String toString = p.toString();
+                System.out.println(toString);
+            }
+        }
     }
 }
