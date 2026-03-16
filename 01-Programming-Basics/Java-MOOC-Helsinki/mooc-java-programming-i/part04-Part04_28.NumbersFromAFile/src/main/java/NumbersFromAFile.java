77595d59c47cd2a48466360f671f7498bf30fc3f
owl-18 <twoblibion18@gmail.com>
2026-03-16 11:30:56 +0700

    feat: complete NumbersFromAFile execrise

--- 01-Programming-Basics/Java-MOOC-Helsinki/mooc-java-programming-i/part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ 01-Programming-Basics/Java-MOOC-Helsinki/mooc-java-programming-i/part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -1,5 +1,6 @@
 
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class NumbersFromAFile {
@@ -14,6 +15,24 @@ public class NumbersFromAFile {
         System.out.print("Upper bound? ");
         int upperBound = Integer.valueOf(scanner.nextLine());
 
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        try (Scanner readFile = new Scanner(Paths.get(file))) {
+            while (readFile.hasNextLine()) {
+                numbers.add(Integer.valueOf(readFile.nextLine()));
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        int count = 0;
+        for (Integer i : numbers) {
+            if (i >= lowerBound && i <= upperBound) {
+                ++count;
+            }
+        }
+
+        System.out.println("Numbers: " + count);
     }
 
 }
