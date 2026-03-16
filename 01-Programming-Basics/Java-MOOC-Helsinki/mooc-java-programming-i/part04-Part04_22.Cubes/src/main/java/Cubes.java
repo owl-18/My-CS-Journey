c883a2c254eddd51a9187a3e3721e060bc0d4820
owl-18 <twoblibion18@gmail.com>
2026-03-16 09:11:26 +0700

    feat: completed Cubes execrise

--- 01-Programming-Basics/Java-MOOC-Helsinki/mooc-java-programming-i/part04-Part04_22.Cubes/src/main/java/Cubes.java
+++ 01-Programming-Basics/Java-MOOC-Helsinki/mooc-java-programming-i/part04-Part04_22.Cubes/src/main/java/Cubes.java
@@ -6,5 +6,14 @@ public class Cubes {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String userInput = scanner.nextLine();
+            if (userInput.equals("end")) {
+                break;
+            }
+            int number = Integer.valueOf(userInput);
+            System.out.println(number * number * number);
+        }
+
     }
 }
