dc12f8f0802e1a94b75e5cc54eae10fe3121a46e
owl-18 <twoblibion18@gmail.com>
2026-03-17 22:43:54 +0700

    feat: completed OneMinute execrise

--- 01-Programming-Basics/Java-MOOC-Helsinki/mooc-java-programming-i/part05-Part05_01.OneMinute/src/main/java/Program.java
+++ 01-Programming-Basics/Java-MOOC-Helsinki/mooc-java-programming-i/part05-Part05_01.OneMinute/src/main/java/Program.java
@@ -5,7 +5,14 @@ public class Program {
 
     public static void main(String[] args) {
         // You can test your program here
+        Timer timer = new Timer();
 
+        int i = 0;
+        while (i < 366410) {
+            System.out.println(timer);
+            timer.advance();
+
+        }
 
     }
 }
