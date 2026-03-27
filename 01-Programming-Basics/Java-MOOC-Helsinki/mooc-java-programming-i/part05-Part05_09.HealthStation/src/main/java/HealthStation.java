70b01a6a9156971d4199fafe408794815fc44dd1
owl-18 <twoblibion18@gmail.com>
2026-03-27 10:27:51 +0700

    feat: completed HealthStation execrise

--- 01-Programming-Basics/Java-MOOC-Helsinki/mooc-java-programming-i/part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ 01-Programming-Basics/Java-MOOC-Helsinki/mooc-java-programming-i/part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,10 +1,16 @@
-
 public class HealthStation {
+  private int count = 0;
 
+  public int weigh(Person person) {
+    this.count++;
+    return person.getWeight();
+  }
 
-    public int weigh(Person person) {
-        // return the weight of the person passed as the parameter
-        return -1;
-    }
+  public void feed(Person person) {
+    person.setWeight(person.getWeight() + 1);
+  }
 
+  public int weighings() {
+    return this.count;
+  }
 }
